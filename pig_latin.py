0d54e6250933206ba87316b30a2a929c99491d10
Brent Bartlett <brent.bartlett1@gmail.com>
2024-11-24 14:53:29 -0800

    build consonant sequence

--- pig_latin.py
+++ pig_latin.py
@@ -7,11 +7,13 @@ original = input("Enter a word:")
 if len(original) > 0 and original.isalpha():
   word = original.lower()
   first = word[0]
-  first_two = word[0:2]
-  first_three = word[0:3]
-  print(first, first_two, first_three)
+  for letter in word:
+    if letter not in ("a", "e", "i", "o", "u"):
+      first = first + letter
+    else:
+      break
   new_word = word + first + pyg
-  new_word = new_word[1:len(new_word)]
+  new_word = new_word[len(first)-1:len(new_word)]
   print(new_word)
 else:
   print("empty")
